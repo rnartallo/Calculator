0b2d18360e9344868b3d3b3f30ec922e73cca013
Moncho Nartallo <Moncho@Monchos-MacBook-Pro.local>
2021-03-08 16:33:10 +0100

    added more operations

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const readline = require('readline-sync');
 console.log('Welcome to the calculator');
+console.log('Please enter an operation:');
+const op = readline.prompt()
 console.log('Please enter a number:');
 const x = parseInt(readline.prompt());
 console.log('Please enter a second number:');
 const y = parseInt(readline.prompt());
-var ans = x*y
+if (op =="+"){
+    var ans = x+y
+}else if(op =="-"){
+    var ans =x-y
+}else if(op =="*"){
+    var ans =x*y
+}else if(op == "/"){
+    var ans = x/y
+}
 console.log('The answer is: '+ String(ans));
