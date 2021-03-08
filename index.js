b03f15097f018a03037c94ea22d37314aa2c602d
Moncho Nartallo <Moncho@Monchos-MacBook-Pro.local>
2021-03-08 16:20:31 +0100

    added a multiplier - single line

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+const readline = require('readline-sync');
 console.log('Welcome to the calculator');
+console.log('Please enter a number:');
+const x = parseInt(readline.prompt());
+console.log('Please enter a second number:');
+const y = parseInt(readline.prompt());
+var ans = x*y
+console.log('The answer is: '+ String(ans));
