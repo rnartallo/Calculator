eeae1a731affca11d258fc54a4b11fd949c6ddc7
Moncho Nartallo <Moncho@MacBook-Pro.localdomain>
2021-06-19 12:55:01 +0100

    Added vowel counting functionality

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const readline = require('readline-sync');
 function printwelcomemessage(){
-console.log('Welcome to the calculator');}
+console.log('Welcome to the calculator');
+console.log('Which calculator do you want?');
+console.log('(1) Arithmetic');
+console.log('(2) Vowel counting');
+const typeofcalc = parseInt(readline.prompt());
+return typeofcalc
+}
 
-function oneCalculation(){console.log('Please enter an operation:');
+function oneArithmeticCalculation(){console.log('Please enter an operation:');
 const op = readline.prompt()
 console.log('How many numbers do you want to ' + String(op)+ '?');
 const n = parseInt(readline.prompt());
@@ -26,5 +32,36 @@ for (let i=1; i<n;i++){
 
 console.log('The answer is: '+ String(total));}
 
-printwelcomemessage()
-oneCalculation()
+function oneVowelCalculation(){
+console.log('Please enter a phrase:')
+var phrase = readline.prompt()
+phrase = phrase.toLowerCase()
+vowels = [0,0,0,0,0] // [a,e,i,o,u]
+for (let i=0;i<phrase.length;i++){
+    if (phrase[i]=='a'){
+        vowels[0]+=1
+    }else if(phrase[i]=='e'){
+        vowels[1]+=1
+    }else if(phrase[i]=='i'){
+        vowels[2]+=1
+    }else if(phrase[i]=='o'){
+        vowels[3]+=1
+    }else if(phrase[i]=='u'){
+        vowels[4]+=1}
+}
+console.log("The vowel breakdown shows:")
+console.log("A: " + String(vowels[0]))
+console.log("E: " + String(vowels[1]))
+console.log("I: " + String(vowels[2]))
+console.log("O: " + String(vowels[3]))
+console.log("U: " + String(vowels[4]))
+}
+
+typeofcalc = printwelcomemessage()
+if (typeofcalc==1){
+    oneArithmeticCalculation()
+}
+else if (typeofcalc==2){
+    oneVowelCalculation()
+}
+
