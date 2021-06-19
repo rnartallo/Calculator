f5e0dd70cb98f7a7f9e226bf6b5a3a54a4dfd0e1
Moncho Nartallo <Moncho@MacBook-Pro.localdomain>
2021-06-19 11:35:13 +0100

    add multiple numbers

--- index.js
+++ index.js
@@ -2,17 +2,24 @@ const readline = require('readline-sync');
 console.log('Welcome to the calculator');
 console.log('Please enter an operation:');
 const op = readline.prompt()
-console.log('Please enter a number:');
-const x = parseInt(readline.prompt());
-console.log('Please enter a second number:');
-const y = parseInt(readline.prompt());
-if (op =="+"){
-    var ans = x+y
-}else if(op =="-"){
-    var ans =x-y
-}else if(op =="*"){
-    var ans =x*y
-}else if(op == "/"){
-    var ans = x/y
+console.log('How many numbers do you want to ' + String(op)+ '?');
+const n = parseInt(readline.prompt());
+arr = Array(n);
+for (let i=0; i<n;i++){
+    console.log('Please enter number ' + String(i+1) + ':');
+    arr[i]=parseInt(readline.prompt());
 }
-console.log('The answer is: '+ String(ans));
+total = arr[0];
+for (let i=1; i<n;i++){
+    if (op =="+"){
+        total+=arr[i]
+    }else if(op =="-"){
+        total-=arr[i]
+    }else if(op =="*"){
+        total*=arr[i]
+    }else if(op == "/"){
+        total/=arr[i]
+    }
+}
+
+console.log('The answer is: '+ String(total));
